9926678741b865cd95b1008f04ef0811f3181424
Max Sporyshev <coockoombra@gmail.com>
2015-12-04 22:19:23 +0300

    Добавил модуль видео, отправляющий сообщения текущего кадра и распознавателя, принимающий команду переключить камеру

--- src/video/main.cpp
+++ src/video/main.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
 
+#include <video/MsgVideoFrame.h>
+#include <video/MsgFoundBin.h>
+#include <video/CmdSwitchCamera.h>
+
 #include <libipc/ipc.h>
 #include <yaml_reader.h>
 
 using namespace std;
+using namespace video;
+
+template<typename Msg>
+void on_receive(const Msg& msg) {
+    cout << msg << endl;
+}
 
 int main(int argc, char** argv) {
     auto comm = ipc::init(argc, argv, "video");
-    cout << "Hello from Video module" << endl;
+
+    auto found_bucket_pub = comm.advertise<MsgFoundBin>();
+    auto frame_pub = comm.advertise<MsgVideoFrame>();
+
+    comm.subscribe_cmd(on_receive<CmdSwitchCamera>);
+
+    ipc::EventLoop loop(10);
+    while (loop.ok()) {
+        MsgVideoFrame img;
+        frame_pub.publish(img);
+
+        MsgFoundBin fb_msg;
+        found_bucket_pub.publish(fb_msg);
+    }
 }
