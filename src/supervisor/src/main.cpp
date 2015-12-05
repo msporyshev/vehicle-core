75c149f74a290f6466d9e56331c8227e27f788ba
bioniwulf <bioniwulf@gmail.com>
2015-12-05 10:36:26 +1000

    Убран лишний отладочный вывод в супервизоре, в дефайн убран период публикации сообщений.

--- src/supervisor/src/main.cpp
+++ src/supervisor/src/main.cpp
@@ -6,6 +6,8 @@
 
 #include "supervisor.h"
 
+#define PUBLISH_PERIOD 0.1
+
 using namespace std;
 
 int main(int argc, char* argv[])
@@ -16,18 +18,14 @@ int main(int argc, char* argv[])
 
     supervisor.init_connection(communicator);
 
-    cout << "try to start" << endl;
+    communicator.create_timer(PUBLISH_PERIOD, &Supervisor::publish_leak, &supervisor);
+    communicator.create_timer(PUBLISH_PERIOD, &Supervisor::publish_compensator, &supervisor);
+    communicator.create_timer(PUBLISH_PERIOD, &Supervisor::publish_devices_status, &supervisor);
+    communicator.create_timer(PUBLISH_PERIOD, &Supervisor::publish_short_circuit, &supervisor);
+    communicator.create_timer(PUBLISH_PERIOD, &Supervisor::publish_adc, &supervisor);
+    communicator.create_timer(PUBLISH_PERIOD, &Supervisor::publish_external_adc, &supervisor);
+    communicator.create_timer(PUBLISH_PERIOD, &Supervisor::publish_depth, &supervisor);
 
-    communicator.create_timer(0.1, &Supervisor::publish_leak, &supervisor);
-    communicator.create_timer(0.1, &Supervisor::publish_compensator, &supervisor);
-    communicator.create_timer(0.1, &Supervisor::publish_devices_status, &supervisor);
-    communicator.create_timer(0.1, &Supervisor::publish_short_circuit, &supervisor);
-    communicator.create_timer(0.1, &Supervisor::publish_adc, &supervisor);
-    communicator.create_timer(0.1, &Supervisor::publish_external_adc, &supervisor);
-    communicator.create_timer(0.1, &Supervisor::publish_depth, &supervisor);
-
-    cout << "try to rotate" << endl;
     ros::spin();
-    cout << "try to end" << endl;
     return 0;
 }
