177078d7edf44d5af211000ea94eaa048e6e9711
Max Sporyshev <coockoombra@gmail.com>
2015-12-15 00:49:17 +1000

    Перевел миссию и видео на новый симейк. Миссия теперь зависит от видео.

--- src/mission/mission.cpp
+++ src/mission/mission.cpp
@@ -11,6 +11,8 @@
 #include <navig/MsgNavigRates.h>
 #include <navig/MsgNavigVelocity.h>
 
+#include <video/MsgFoundBin.h>
+
 using namespace std;
 
 const string Mission::NODE_NAME = "mission";
@@ -38,6 +40,8 @@ void Mission::init_ipc()
     communicator_.subscribe("navig", &Mission::handle_message<navig::MsgNavigPosition>, this);
     communicator_.subscribe("navig", &Mission::handle_message<navig::MsgNavigRates>, this);
     communicator_.subscribe("navig", &Mission::handle_message<navig::MsgNavigVelocity>, this);
+
+    communicator_.subscribe("video", &Mission::handle_message<video::MsgFoundBin>, this);
     /**
         Вот здесь нужно будет вписать подписку на сообщения от модуля видео
     */
@@ -51,8 +55,8 @@ void Mission::publish_commands()
     motion_.fix_heading(static_cast<float>(rand()) / static_cast<float>(RAND_MAX / 360), rand(), WaitMode::DONT_WAIT);
     motion_.fix_depth(static_cast<float>(rand()) / static_cast<float>(RAND_MAX / 30), rand(), WaitMode::DONT_WAIT);
     motion_.fix_pitch();
-    motion_.fix_position(MakePoint2(static_cast<double>(rand()) / static_cast<double>(RAND_MAX / 100), 
-        static_cast<double>(rand()) / static_cast<double>(RAND_MAX / 100)), static_cast<MoveMode>(rand() % 2), 
+    motion_.fix_position(MakePoint2(static_cast<double>(rand()) / static_cast<double>(RAND_MAX / 100),
+        static_cast<double>(rand()) / static_cast<double>(RAND_MAX / 100)), static_cast<MoveMode>(rand() % 2),
         rand(), WaitMode::DONT_WAIT);
 }
 
@@ -60,7 +64,7 @@ int main(int argc, char* argv[])
 {
     auto communicator = ipc::init(argc, argv, Mission::NODE_NAME);
     Mission mission(communicator);
-    
+
     ipc::EventLoop loop(10);
     while (loop.ok()) {
         mission.publish_commands();
