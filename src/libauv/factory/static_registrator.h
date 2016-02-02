ba985954ff77bcefc04571c22da078142ed658af
Max Sporyshev <coockoombra@gmail.com>
2016-02-02 17:09:00 +1000

    Фикс в интерфейсе фабрики. Добавлена фабрика, инициализирующая свои объекты во время запроса, а не во время регистрации

--- src/libauv/factory/static_registrator.h
+++ src/libauv/factory/static_registrator.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <map>
+#include <memory>
+#include <string>
 
 template<typename T>
 class Singleton
@@ -25,24 +27,64 @@ public:
     template<typename Class>
     void reg(std::string name, Class* obj_ptr)
     {
-        obj[name] = std::shared_ptr<Class>(obj_ptr);
+        obj_[name] = std::shared_ptr<Class>(obj_ptr);
+    }
+
+    std::shared_ptr<BaseClass> get(std::string name)
+    {
+        return obj_.at(name);
+    }
+
+protected:
+    std::map<std::string, std::shared_ptr<BaseClass> > obj_;
+};
+
+template<typename BaseClass, typename InitFunc>
+class LazyFactory
+{
+public:
+    void reg(std::string name, InitFunc cons)
+    {
+        constructor_[name] = cons;
+    }
+
+    template<typename ... Args>
+    std::shared_ptr<BaseClass> init(std::string name, Args ... args)
+    {
+        auto ptr = std::shared_ptr<BaseClass>(constructor_.at(name)(args...));
+        obj_[name] = ptr;
+        return ptr;
+    }
+
+    template<typename ... Args>
+    void init_all(Args ... args)
+    {
+        for (auto& cons : constructor_) {
+            obj_.insert(make_pair(cons.first, std::shared_ptr<BaseClass>(cons.second(args...))));
+        }
     }
 
     BaseClass& get(std::string name)
     {
-        return *obj.at(name);
+        return *obj_.at(name);
     }
 
 protected:
-    std::map<std::string, std::shared_ptr<BaseClass> > obj;
+    std::map<std::string, std::shared_ptr<BaseClass> > obj_;
+    std::map<std::string, InitFunc> constructor_;
 };
 
 
-template<typename Factory, typename RegisterClass>
+template<typename FactoryImpl, typename Register>
 struct StaticRegistrator
 {
-    StaticRegistrator(std::string name, RegisterClass* obj_ptr)
+    StaticRegistrator(std::string name, Register obj)
     {
-        Singleton<Factory>::instance().reg(name, obj_ptr);
+        Singleton<FactoryImpl>::instance().reg(name, obj);
+    }
+
+    StaticRegistrator(std::string name, Register* obj_ptr)
+    {
+        Singleton<FactoryImpl>::instance().reg(name, obj_ptr);
     }
 };
