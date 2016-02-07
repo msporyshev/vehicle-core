d4efd90d32ef2357e8e1cc2c10580447be7ec572
bioniwulf <bioniwulf@gmail.com>
2016-02-07 18:44:59 +1000

    Добавлены ноды для получения данных для калибровки и загрузки калибровочных коэффициентов.

--- src/compass/src/x-sens/calibration/mti_compass_calibration_data_collect.cpp
+++ src/compass/src/x-sens/calibration/mti_compass_calibration_data_collect.cpp
@@ -10,358 +10,136 @@
 
 
 // Идентификатор программы
-#define TASK_NAME ( "mti_compass_calibration_data_collect" )
+// #define TASK_NAME ( "mti_compass_calibration_data_collect" )
 
 // Длительность процедуры калибровки
-#define CALIBRATION_PERIOD ( 60.0 * 4 )
+#define CALIBRATION_PERIOD (60.0 * 4)
+
+// Период таймера в микросекундах
+#define TIMER_PERIOD (10000)
 
 // Плохой файловый дескриптор
-#define INVALID_FILE_DESCRIPTOR ( -1 )
+#define INVALID_FILE_DESCRIPTOR (-1)
 
 //------------------------------------------------------------------------------
 
-#include <stdio.h>              // Standard Input / Output
-#include <stdlib.h>             // General Utilities
-#include <string.h>             // String Handling
-#include <unistd.h>             // Symbolic Constants
-#include <stdarg.h>             // Variable Arguments
+#include "compass/x-sens/MTI.h"
+
+#include <libipc/ipc.h>
+#include "ros/ros.h"
+#include <ros/package.h>
+#include <boost/program_options.hpp>
+
 #include <fcntl.h>              // File Control Operations
-#include <termios.h>            // General Terminal Interface
-#include <sys/ioctl.h>          //
 
-//------------------------------------------------------------------------------
+std::string port;
+std::string file_name;
+std::string file_path;
+int baundrate;
 
-#include "utils/basic.h"              // Общие функции
+int com_descriptor;
 
-//------------------------------------------------------------------------------
-// Команды управления компасом MTi XSens
+using namespace std;
+namespace po = boost::program_options;
 
-unsigned char GoToConfig[] = {0xFA, 0xFF, 0x30, 0x00, 0xD1};
-unsigned char SetOutputMode[] = {0xFA, 0xFF, 0xD0, 0x02, 0x40, 0x00, 0xEF};
-unsigned char SetOutputSettings[] = {0xFA, 0xFF, 0xD2, 0x04, 0x00, 0x00, 0x00, 0x01, 0x2A};
-unsigned char Reset[] = {0xFA, 0xFF, 0x40, 0x00, 0xC1};
-
-
-//------------------------------------------------------------------------------
-// Период таймера в микросекундах
-#define TIMER_PERIOD ( 10000 )
-
-//==============================================================================
-//
-// Работа с COM-портом (открытие, настройка, закрытие)
-//
-//==============================================================================
-
-
-//------------------------------------------------------------------------------
-//
-// Открытие COM-порта и его настройка для работы с инерциальным
-// измерительным модулем MTi (Xsens Technologies B.V.)
-//
-//------------------------------------------------------------------------------
-int MTI_COM_open ( const char *COM_name, int baudrate )
+void program_options_init(int argc, char** argv) 
 {
-  // Файловый дескриптор для COM-порта
-  int fd;
+    po::options_description desc("Usage");
+    desc.add_options()
+      ("help,h", "Produce help message.")
+      ("port,c", po::value(&port),
+          "Set COM-port name (e.g. /dev/ttyS0).")
+      ("baundrate,b", po::value(&baundrate),
+          "Set COM-port baundrate (e.g. -b 115200).")
+      ("file,f", po::value(&file_name),
+          "Set filename for data.");
 
-  // Открытие COM-порта
-  fd = open ( COM_name, O_RDWR | O_NOCTTY | O_NDELAY );
-
-  // Результат открытия COM-порта
-  if ( fd == INVALID_FILE_DESCRIPTOR )
-  {
-    printf ( "\nНе удалось открыть COM-порт %s.\n\n", COM_name );
-    return ( INVALID_FILE_DESCRIPTOR );
-  }
-  else
-    printf ( "\nCOM-порт %s открыт.\n\n", COM_name );
-
-  //----------------------------------------------------------------------------
-  // Настройка COM-порта
-
-  // Структура с настройками COM-порта
-  struct termios options;
-
-  // Получение текущих настроек COM-порта
-  if ( tcgetattr ( fd , &options ) != 0 )
-  {
-    puts ( "\nНе удалось получить текущие настройки COM-порта.\n\nCOM-порт закрыт.\n" );
-    close ( fd );
-    return ( INVALID_FILE_DESCRIPTOR );
-  }
-
-  // Определение скорости обмена
-  switch ( baudrate )
-  {
-    case 2400:          baudrate = B2400; break;
-    case 4800:          baudrate = B4800; break;
-    case 9600:          baudrate = B9600; break;
-    case 19200:         baudrate = B19200; break;
-    case 38400:         baudrate = B38400; break;
-    case 57600:         baudrate = B57600; break;
-    case 115200:        baudrate = B115200; break;
-    case 230400:        baudrate = B230400; break;
-    case 460800:        baudrate = B460800; break;
-    default:
-      puts ( "\nНе удалось настроить COM-порт для работы на требуемой скорости.\n\nCOM-порт закрыт.\n" );
-      close ( fd );
-      return ( INVALID_FILE_DESCRIPTOR );
-  }
-
-  // Установка скорости работы COM-порта
-  if ( cfsetspeed( &options, baudrate ) != 0 )
-  {
-    puts ( "\nНе удалось настроить COM-порт для работы на требуемой скорости.\n\nCOM-порт закрыт.\n" );
-    close ( fd );
-    return ( INVALID_FILE_DESCRIPTOR );
-  }
-
-  // Управляющие опции (c_cflag)
-
-  options.c_cflag |= ( CLOCAL | CREAD );        // Обязательные опции
-
-  options.c_cflag &= ~CSIZE;    // Маскирование битов размера симвоов
-  //options.c_cflag |= CS5;       // 5 бит данных
-  //options.c_cflag |= CS6;       // 6 бит данных
-  //options.c_cflag |= CS7;       // 7 бит данных
-  options.c_cflag |= CS8;       // 8 бит данных
-
-  //options.c_cflag |= PARENB;    // Использовать бит четности
-  options.c_cflag &= ~PARENB;   // Не использовать бит четности
-
-  //options.c_cflag |= PARODD;    // Проверка на нечетность
-  //options.c_cflag &= ~PARODD;   // Проверка на четность
-
-  options.c_cflag |= CSTOPB;    // Два стоповых бита
-  //options.c_cflag &= ~CSTOPB;   // Один стоповый бит
-
-  //options.c_cflag |= CRTSCTS;   // Включение аппаратного управления потоком
-  options.c_cflag &= ~CRTSCTS;  // Отключение аппаратного управления потоком
-
-  // Опции ввода (c_iflag)
-
-  //options.c_iflag |= ( IXON | IXOFF | IXANY );  // Включение программного управления потоком
-  options.c_iflag &= ~( IXON | IXOFF | IXANY ); // Отключение программного управления потоком
-
-  options.c_iflag &= ~( INLCR | ICRNL | IGNCR | IUCLC | IMAXBEL | BRKINT );
-
-  // Опции вывода (c_oflag)
-
-  //options.c_oflag |= OPOST;    // Режим обработанного вывода
-  options.c_oflag &= ~OPOST;    // Режим необработанного вывода. Остальные биты поля c_oflag игнорируются.
-
-  // Локальные опции (c_lflag)
-
-  options.c_lflag &= ~( ICANON | ECHO | ECHOE | ISIG );
-
-  // Применение данных настроек COM-порта
-  if ( tcsetattr ( fd , TCSANOW, &options ) != 0 )
-  {
-    puts ( "\nНе удалось настроить COM-порт.\n\nCOM-порт закрыт.\n" );
-    close ( fd );
-    return ( INVALID_FILE_DESCRIPTOR );
-  }
-
-  return ( fd );
+    std::string base_path = ros::package::getPath("compass") + "/calibration_data";
+    if(file_name.size() == 0) {
+        file_name = "/MT_data.mtb";
+    } 
+    file_path = base_path + file_name;
+    
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        exit(EXIT_SUCCESS);
+    }
 }
 
-
-//------------------------------------------------------------------------------
-//
-// Закрытие COM-порта
-//
-//------------------------------------------------------------------------------
-void MTI_COM_close ( int *fd )
-{
-  if ( *fd != INVALID_FILE_DESCRIPTOR )
-  {
-    puts ( "\nCOM-порт закрыт.\n" );
-    close( *fd );
-    *fd = INVALID_FILE_DESCRIPTOR;
-  }
-  else
-  {
-    puts ( "\nПри закрытии COM-порта произошла ошибка. Возможно COM-порт уже закрыт.\n" );
-  }
-}
-
-
-//------------------------------------------------------------------------------
-//
-// Вывод заголовка программы
-//
-//------------------------------------------------------------------------------
-void print_program_header ( )
-{
-  puts ( "                                        " );
-  puts ( "----------------------------------------" );
-  puts ( "                                        " );
-  puts ( "  MTi compass calibration data collect  ");
-  puts ( "                                        " );
-  puts ( "             August 25, 2013            " );
-  puts ( "                                        " );
-  puts ( "----------------------------------------" );
-  puts ( "                                        " );
-}
-
-
-//------------------------------------------------------------------------------
-//
-// Главная функция
-//
-//------------------------------------------------------------------------------
 int main ( int argc, char *argv[] )
 {
-  // Файловый дескриптор для работы с COM-портом
-  int COM_descriptor = INVALID_FILE_DESCRIPTOR;
+    program_options_init(argc, argv);
 
-  // Параметры COM-порта
-  char COM_name[32] = "/dev/ttyS0";
-  int COM_baudrate = 57600;
-
-  // Файловый дескриптор для файла данных
-  int fd_raw = INVALID_FILE_DESCRIPTOR;
-
-  // Буфер, в который считываются принимаемые с COM-порта данные
-  unsigned char buffer[8192];
-
-  // Число реально прочитанных байт
-  int n;
-
-  // Метка времени начала сбора данных для калибровки компаса
-  double start_time;
-
-  // Переменная для разборщика параметров
-  int opt;
-
-  // Обработка параметров, переданных программе при запуске
-  while ( ( opt = getopt ( argc, argv, "c:C:b:B:?hH" ) ) != -1 )
-  {
-    switch ( opt )
-    {
-      // Вывод справки по параметрам запуска драйвера
-      case '?':
-      case 'h':
-      case 'H':
-        // Вывод заголовка программы
-        print_program_header ( );
-
-        puts ( "Options description:" );
-        puts ( "  c <arg>  - set COM-port name" );
-        puts ( "  b <arg>  - set COM-port baudrate" );
-        puts ( "" );
-        return ( EXIT_SUCCESS );
-        break;
-
-      // Задание имени COM-порта
-      case 'c':
-      case 'C':
-        strcpy ( COM_name, optarg );
-        break;
-
-      // Задание скорости работы COM-порта
-      case 'b':
-      case 'B':
-        COM_baudrate = atoi ( optarg );
-        break;
-
-      default:
-        puts ( "ERROR! Unsupported parameter" );
+    if (port.size() == 0 || baundrate == 0) {
+        ROS_ERROR_STREAM("The settings have not been established. Program close.");
+        return (EXIT_FAILURE);
     }
-  }
 
-  // Вывод заголовка программы
-  print_program_header ( );
+    MTI mti;
+    
+    // Буфер, в который считываются принимаемые с COM-порта данные
+    unsigned char buffer[8192];
 
-  printf ( "COM_name = %s\n", COM_name );
+    com_descriptor = mti.MTI_COM_open(port.c_str(), baundrate);
 
-  printf ( "COM_baudrate = %d\n", COM_baudrate );
+    // Файловый дескриптор для файла данных
+    int fd_raw = INVALID_FILE_DESCRIPTOR;
 
-  // Открытие COM-порта и его настройка
-  COM_descriptor = MTI_COM_open ( COM_name, COM_baudrate );
+    // Создание файла с сырыми данными для калибровки компаса
+    fd_raw = open(file_path.c_str(), O_WRONLY | O_CREAT);
 
-  // Если не удалось открыть COM-порт
-  if ( COM_descriptor == INVALID_FILE_DESCRIPTOR )
-  {
-    puts ( "ERROR: MTI_COM_open" );
-    puts ( "Program was terminated" );
+    if (fd_raw == INVALID_FILE_DESCRIPTOR) {
+        ROS_ERROR_STREAM("ERROR: Can not create output file" <<  file_name);
+        ROS_ERROR_STREAM("Program was terminated");
 
-    return ( EXIT_FAILURE );
-  }
-
-  // Создание файла с сырыми данными для калибровки компаса
-  fd_raw = open ( "MT_data.mtb", O_WRONLY | O_CREAT );
-
-  if ( fd_raw == INVALID_FILE_DESCRIPTOR )
-  {
-    puts ( "ERROR: Can not create output file MT_data.mtb" );
-    puts ( "Program was terminated" );
-
-    // Закрытие COM-порта
-    MTI_COM_close ( &COM_descriptor );
-
-    return ( EXIT_FAILURE );
-  }
-
-  puts ( "File MT_data.mtb was created" );
-
-  n = write ( COM_descriptor, GoToConfig, sizeof ( GoToConfig ) );
-  puts ( "GoToConfig" );
-  usleep ( 500000 );
-
-  n = write ( COM_descriptor, SetOutputMode, sizeof ( SetOutputMode ) );
-  puts ( "SetOutputMode" );
-  usleep ( 500000 );
-
-  n = write ( COM_descriptor, SetOutputSettings, sizeof ( SetOutputSettings ) );
-  puts ( "SetOutputSettings" );
-  usleep ( 500000 );
-
-  // Очистка входного буфера COM-порта от полученных ответов
-  do
-  {
-    n = read ( COM_descriptor, buffer, 8192 );
-  } while ( n > 0 );
-
-  puts ( "Buffer flush" );
-
-  puts ( "Start calibration" );
-
-  n = write ( COM_descriptor, Reset, sizeof ( Reset ) );
-  puts ( "Reset" );
- 
-  start_time = fixate_time ( );
-
-  while ( fixate_time ( ) - start_time < CALIBRATION_PERIOD )
-  {
-    n = read ( COM_descriptor, buffer, 8192 );
-
-    if ( n > 0 )
-    {
-      write ( fd_raw, buffer, n );
+        // Закрытие COM-порта
+        mti.MTI_COM_close(&com_descriptor);
+        return (EXIT_FAILURE);
     }
-    else
-    {
-      usleep ( TIMER_PERIOD );
+
+    mti.MTI_start_calibrate(com_descriptor);
+    ROS_INFO_STREAM("Start calibration");
+
+    // Очистка входного буфера COM-порта от полученных ответов
+    int n;
+    do {
+        n = read (com_descriptor, buffer, 8192);
+    } while (n > 0);
+
+    ROS_INFO_STREAM("Buffer flush");
+
+
+    mti.MTI_reset(com_descriptor);
+    ROS_INFO_STREAM("Reset compass");
+
+    ros::Time::init();
+    ros::Time start_time = ros::Time::now();
+    ros::Duration calibration_period = ros::Duration(CALIBRATION_PERIOD);
+
+    while (ros::Time::now() - start_time < calibration_period) {
+        n = read (com_descriptor, buffer, 8192);
+
+        if (n > 0) {
+            write (fd_raw, buffer, n);
+        } else {
+            usleep (TIMER_PERIOD);
+        }
     }
-  }
 
-  n = write ( COM_descriptor, GoToConfig, sizeof ( GoToConfig ) );
-  puts ( "GoToConfig" );
-  usleep ( 500000 );
+    mti.MTI_stop_calibrate (com_descriptor);
+    ROS_INFO_STREAM("Stop calibration");
 
-  // Дописываем в файл все данные из приемного буфера COM-порта
-  while ( ( n = read ( COM_descriptor, buffer, 8192 ) ) > 0 )
-  {
-    write ( fd_raw, buffer, n );
-  }
+      // Дописываем в файл все данные из приемного буфера COM-порта
+    while ((n = read (com_descriptor, buffer, 8192)) > 0) {
+        write (fd_raw, buffer, n);
+    }
 
-  close ( fd_raw );
-  puts ( "File MT_data.mtb was closed" );
+    close(fd_raw);
+    ROS_INFO_STREAM("File MT_data.mtb was closed");
 
-  // Закрытие COM-порта
-  MTI_COM_close ( &COM_descriptor );
-
-  return ( EXIT_SUCCESS );
+    mti.MTI_COM_close (&com_descriptor);
+    ROS_INFO_STREAM("Compass was closed");
 }
-
--- src/compass/src/x-sens/calibration/mti_compass_calibration_data_store.cpp
+++ src/compass/src/x-sens/calibration/mti_compass_calibration_data_store.cpp
@@ -8,331 +8,115 @@
 //
 //==============================================================================
 
+#include "compass/x-sens/MTI.h"
 
-// Идентификатор программы
-#define TASK_NAME ( "mti_compass_calibration_data_store" )
+#include <libipc/ipc.h>
+#include "ros/ros.h"
+#include <boost/program_options.hpp>
+
+#include <fcntl.h>              // File Control Operations
 
 // Плохой файловый дескриптор
-#define INVALID_FILE_DESCRIPTOR ( -1 )
+#define INVALID_FILE_DESCRIPTOR (-1)
 
-//------------------------------------------------------------------------------
+std::string port;
+std::string file_name;
+int baundrate;
 
-#include <stdio.h>              // Standard Input / Output
-#include <stdlib.h>             // General Utilities
-#include <string.h>             // String Handling
-#include <unistd.h>             // Symbolic Constants
-#include <stdarg.h>             // Variable Arguments
-#include <fcntl.h>              // File Control Operations
-#include <termios.h>            // General Terminal Interface
-#include <sys/ioctl.h>          //
+int com_descriptor;
 
-//------------------------------------------------------------------------------
+using namespace std;
+namespace po = boost::program_options;
 
-#include "utils/basic.h"              // Общие функции
-
-//------------------------------------------------------------------------------
-// Команды управления компасом MTi XSens
-
-unsigned char GoToConfig[] = {0xFA, 0xFF, 0x30, 0x00, 0xD1};
-unsigned char Reset[] = {0xFA, 0xFF, 0x40, 0x00, 0xC1};
-
-
-//==============================================================================
-//
-// Работа с COM-портом (открытие, настройка, закрытие)
-//
-//==============================================================================
-
-
-//------------------------------------------------------------------------------
-//
-// Открытие COM-порта и его настройка для работы с инерциальным
-// измерительным модулем MTi (Xsens Technologies B.V.)
-//
-//------------------------------------------------------------------------------
-int MTI_COM_open ( const char *COM_name, int baudrate )
+void program_options_init(int argc, char** argv) 
 {
-  // Файловый дескриптор для COM-порта
-  int fd;
+    po::options_description desc("Usage");
+    desc.add_options()
+      ("help,h", "Produce help message.")
+      ("port,c", po::value(&port),
+          "Set COM-port name (e.g. /dev/ttyS0).")
+      ("baundrate,b", po::value(&baundrate),
+          "Set COM-port baundrate (e.g. -b 115200).")
+      ("file,f", po::value(&file_name),
+          "Set filename for stored data.");
 
-  // Открытие COM-порта
-  fd = open ( COM_name, O_RDWR | O_NOCTTY | O_NDELAY );
+    if(file_name.size() == 0) {
+        file_name = "mfmResults.bin";
+    } 
 
-  // Результат открытия COM-порта
-  if ( fd == INVALID_FILE_DESCRIPTOR )
-  {
-    printf ( "\nНе удалось открыть COM-порт %s.\n\n", COM_name );
-    return ( INVALID_FILE_DESCRIPTOR );
-  }
-  else
-    printf ( "\nCOM-порт %s открыт.\n\n", COM_name );
-
-  //----------------------------------------------------------------------------
-  // Настройка COM-порта
-
-  // Структура с настройками COM-порта
-  struct termios options;
-
-  // Получение текущих настроек COM-порта
-  if ( tcgetattr ( fd , &options ) != 0 )
-  {
-    puts ( "\nНе удалось получить текущие настройки COM-порта.\n\nCOM-порт закрыт.\n" );
-    close ( fd );
-    return ( INVALID_FILE_DESCRIPTOR );
-  }
-
-  // Определение скорости обмена
-  switch ( baudrate )
-  {
-    case 2400:          baudrate = B2400; break;
-    case 4800:          baudrate = B4800; break;
-    case 9600:          baudrate = B9600; break;
-    case 19200:         baudrate = B19200; break;
-    case 38400:         baudrate = B38400; break;
-    case 57600:         baudrate = B57600; break;
-    case 115200:        baudrate = B115200; break;
-    case 230400:        baudrate = B230400; break;
-    case 460800:        baudrate = B460800; break;
-    default:
-      puts ( "\nНе удалось настроить COM-порт для работы на требуемой скорости.\n\nCOM-порт закрыт.\n" );
-      close ( fd );
-      return ( INVALID_FILE_DESCRIPTOR );
-  }
-
-  // Установка скорости работы COM-порта
-  if ( cfsetspeed( &options, baudrate ) != 0 )
-  {
-    puts ( "\nНе удалось настроить COM-порт для работы на требуемой скорости.\n\nCOM-порт закрыт.\n" );
-    close ( fd );
-    return ( INVALID_FILE_DESCRIPTOR );
-  }
-
-  // Управляющие опции (c_cflag)
-
-  options.c_cflag |= ( CLOCAL | CREAD );        // Обязательные опции
-
-  options.c_cflag &= ~CSIZE;    // Маскирование битов размера симвоов
-  //options.c_cflag |= CS5;       // 5 бит данных
-  //options.c_cflag |= CS6;       // 6 бит данных
-  //options.c_cflag |= CS7;       // 7 бит данных
-  options.c_cflag |= CS8;       // 8 бит данных
-
-  //options.c_cflag |= PARENB;    // Использовать бит четности
-  options.c_cflag &= ~PARENB;   // Не использовать бит четности
-
-  //options.c_cflag |= PARODD;    // Проверка на нечетность
-  //options.c_cflag &= ~PARODD;   // Проверка на четность
-
-  options.c_cflag |= CSTOPB;    // Два стоповых бита
-  //options.c_cflag &= ~CSTOPB;   // Один стоповый бит
-
-  //options.c_cflag |= CRTSCTS;   // Включение аппаратного управления потоком
-  options.c_cflag &= ~CRTSCTS;  // Отключение аппаратного управления потоком
-
-  // Опции ввода (c_iflag)
-
-  //options.c_iflag |= ( IXON | IXOFF | IXANY );  // Включение программного управления потоком
-  options.c_iflag &= ~( IXON | IXOFF | IXANY ); // Отключение программного управления потоком
-
-  options.c_iflag &= ~( INLCR | ICRNL | IGNCR | IUCLC | IMAXBEL | BRKINT );
-
-  // Опции вывода (c_oflag)
-
-  //options.c_oflag |= OPOST;    // Режим обработанного вывода
-  options.c_oflag &= ~OPOST;    // Режим необработанного вывода. Остальные биты поля c_oflag игнорируются.
-
-  // Локальные опции (c_lflag)
-
-  options.c_lflag &= ~( ICANON | ECHO | ECHOE | ISIG );
-
-  // Применение данных настроек COM-порта
-  if ( tcsetattr ( fd , TCSANOW, &options ) != 0 )
-  {
-    puts ( "\nНе удалось настроить COM-порт.\n\nCOM-порт закрыт.\n" );
-    close ( fd );
-    return ( INVALID_FILE_DESCRIPTOR );
-  }
-
-  return ( fd );
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        exit(EXIT_SUCCESS);
+    }
 }
 
-
-//------------------------------------------------------------------------------
-//
-// Закрытие COM-порта
-//
-//------------------------------------------------------------------------------
-void MTI_COM_close ( int *fd )
-{
-  if ( *fd != INVALID_FILE_DESCRIPTOR )
-  {
-    puts ( "\nCOM-порт закрыт.\n" );
-    close( *fd );
-    *fd = INVALID_FILE_DESCRIPTOR;
-  }
-  else
-  {
-    puts ( "\nПри закрытии COM-порта произошла ошибка. Возможно COM-порт уже закрыт.\n" );
-  }
-}
-
-
-//------------------------------------------------------------------------------
-//
-// Вывод заголовка программы
-//
-//------------------------------------------------------------------------------
-void print_program_header ( )
-{
-  puts ( "                                        " );
-  puts ( "----------------------------------------" );
-  puts ( "                                        " );
-  puts ( "   MTi compass calibration data store   ");
-  puts ( "                                        " );
-  puts ( "             August 25, 2013            " );
-  puts ( "                                        " );
-  puts ( "----------------------------------------" );
-  puts ( "                                        " );
-}
-
-
-//------------------------------------------------------------------------------
-//
-// Главная функция
-//
-//------------------------------------------------------------------------------
 int main ( int argc, char *argv[] )
 {
-  // Файловый дескриптор для работы с COM-портом
-  int COM_descriptor = INVALID_FILE_DESCRIPTOR;
+    program_options_init(argc, argv);
 
-  // Параметры COM-порта
-  char COM_name[32] = "/dev/ttyS0";
-  int COM_baudrate = 57600;
-
-  // Файловый дескриптор для файла данных
-  int fd_calibration = INVALID_FILE_DESCRIPTOR;
-
-  // Буфер, в который считываются данные
-  unsigned char buffer[8192];
-
-  // Число реально прочитанных байт
-  int n;
-
-  // Переменная для разборщика параметров
-  int opt;
-
-  // Обработка параметров, переданных программе при запуске
-  while ( ( opt = getopt ( argc, argv, "c:C:b:B:?hH" ) ) != -1 )
-  {
-    switch ( opt )
-    {
-      // Вывод справки по параметрам запуска драйвера
-      case '?':
-      case 'h':
-      case 'H':
-        // Вывод заголовка программы
-        print_program_header ( );
-
-        puts ( "Options description:" );
-        puts ( "  c <arg>  - set COM-port name" );
-        puts ( "  b <arg>  - set COM-port baudrate" );
-        puts ( "" );
-        return ( EXIT_SUCCESS );
-        break;
-
-      // Задание имени COM-порта
-      case 'c':
-      case 'C':
-        strcpy ( COM_name, optarg );
-        break;
-
-      // Задание скорости работы COM-порта
-      case 'b':
-      case 'B':
-        COM_baudrate = atoi ( optarg );
-        break;
-
-      default:
-        puts ( "ERROR! Unsupported parameter" );
+    if (port.size() == 0 || baundrate == 0) {
+        ROS_ERROR_STREAM("The settings have not been established. Program close.");
+        return (EXIT_FAILURE);
     }
-  }
 
-  // Вывод заголовка программы
-  print_program_header ( );
+    MTI mti;
+    
+    // Буфер, в который считываются принимаемые с COM-порта данные
+    unsigned char buffer[8192];
 
-  printf ( "COM_name = %s\n", COM_name );
+    com_descriptor = mti.MTI_COM_open(port.c_str(), baundrate);
 
-  printf ( "COM_baudrate = %d\n", COM_baudrate );
-
-  // Открытие COM-порта и его настройка
-  COM_descriptor = MTI_COM_open ( COM_name, COM_baudrate );
-
-  // Если не удалось открыть COM-порт
-  if ( COM_descriptor == INVALID_FILE_DESCRIPTOR )
-  {
-    puts ( "ERROR: MTI_COM_open" );
-    puts ( "Program was terminated" );
-
-    return ( EXIT_FAILURE );
-  }
-
-  puts ( "Start of calibration data store" );
+    // Файловый дескриптор для файла данных
+    int fd_calibration = INVALID_FILE_DESCRIPTOR;
 
   // Открытие файла с данными для калибровки компаса
-  fd_calibration = open ( "mfmResults.bin", O_RDONLY );
+    fd_calibration = open ( "mfmResults.bin", O_RDONLY );
 
-  if ( fd_calibration == INVALID_FILE_DESCRIPTOR )
-  {
-    puts ( "ERROR: Calibration file mfmResults.bin is absent" );
-    puts ( "Program was terminated" );
+    if ( fd_calibration == INVALID_FILE_DESCRIPTOR ) {
+        ROS_ERROR_STREAM("ERROR: Calibration file " << file_name << " is absent");
+        ROS_ERROR_STREAM("Program was terminated");
+
+        // Закрытие COM-порта
+        mti.MTI_COM_close (&com_descriptor);
+        return (EXIT_FAILURE);
+    }
+
+    ROS_ERROR_STREAM("Calibration file mfmResults.bin was opened");
+
+    int n = read (fd_calibration, buffer, 65);
+
+    if (n != 65) {
+        ROS_ERROR_STREAM("ERROR: Calibration file " << file_name << " reading, size: " << n);
+
+        close (fd_calibration);
+        return (EXIT_FAILURE);
+    }
+    ROS_INFO_STREAM("Calibration file reading: OK");
+
+    n = write (com_descriptor, mti.GoToConfig, mti.GoToConfig_size);
+    usleep (500000);
+
+    n = write (com_descriptor, buffer, 65);
+    usleep (500000);
+
+    if (n != 65) {
+        ROS_ERROR_STREAM("ERROR: Calibration data writing error");
+
+        // Закрытие COM-порта
+        mti.MTI_COM_close(&com_descriptor);
+        return (EXIT_FAILURE);
+    }
+
+    ROS_INFO_STREAM("Calibration was successfully finished");
+
+    close (fd_calibration);
+    ROS_INFO_STREAM("Calibration file was closed");
 
     // Закрытие COM-порта
-    MTI_COM_close ( &COM_descriptor );
+    mti.MTI_COM_close (&com_descriptor);
 
-    return ( EXIT_FAILURE );
-  }
-
-  puts ( "Calibration file mfmResults.bin was opened" );
-
-  n = read ( fd_calibration, buffer, 65 );
-
-  if ( n != 65 )
-  {
-    puts ( "ERROR: Calibration file mfmResults.bin reading" );
-
-    close ( fd_calibration );
-    return ( EXIT_FAILURE );
-  }
-
-  puts ( "Calibration file mfmResults.bin reading: OK" );
-
-  n = write ( COM_descriptor, GoToConfig, sizeof ( GoToConfig ) );
-  puts ( "GoToConfig" );
-  usleep ( 500000 );
-
-  n = write ( COM_descriptor, buffer, 65 );
-  puts ( "StoreCalibrationData" );
-  usleep ( 500000 );
-
-  if ( n != 65 )
-  {
-    puts ( "ERROR: Calibration data writing error" );
-
-    // Закрытие COM-порта
-    MTI_COM_close ( &COM_descriptor );
-
-    return (EXIT_FAILURE);
-  }
-
-  puts ( "Calibration was successfully finished" );
-
-  close ( fd_calibration );
-  puts ( "Calibration file mfmResults.bin was closed" );
-
-  // Закрытие COM-порта
-  MTI_COM_close ( &COM_descriptor );
-
-  return ( EXIT_SUCCESS );
+    return (EXIT_SUCCESS);
 }
-
