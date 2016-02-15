4f17c3883446eb89a45f9d8d69e148c3c65f9955
YoshihiroHirano <leavinghell@mail.ru>
2016-02-16 09:35:56 +1000

    Забыл запушить функцию минимаксной регрессии для распознавалки полосы

--- src/video/stripe.h
+++ src/video/stripe.h
@@ -8,6 +8,18 @@
 #include "common.h"
 #include "rec_factory.h"
 
+using Segment = std::pair<cv::Point2d, cv::Point2d>;
+
+struct Stripe {
+    Segment line, width;
+
+    Stripe(Segment line = Segment(), Segment width = Segment()): line(line), width(width) {}
+
+    double length() {
+        return norm(line.first - line.second);
+    }
+};
+
 class StripeRecognizer
 {
 public:
@@ -21,6 +33,9 @@ private:
     video::MsgFoundBin fill_msg(const std::vector<cv::Point>& stripes);
     std::vector<cv::Point> find_stripe(cv::Mat& img);
     std::vector<Stripe> find_stripe_on_bin_img(cv::Mat& img);
+
+    // Для данного массива точек находится минимаксная регрессия, которая "обрезается" в соответствии с размерами контура.
+    Stripe min_max_regression_segment(const std::vector<cv::Point> poly, double EPS = 1e-4);
 };
 
 REGISTER_RECOGNIZER(StripeRecognizer, stripe);
