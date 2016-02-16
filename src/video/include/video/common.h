76599b14deb646304ceb7a86292c0e2b680a587c
YoshihiroHirano <leavinghell@mail.ru>
2016-02-16 22:50:42 +1000

    Добавил красный и зеленый цвета

--- src/video/include/video/common.h
+++ src/video/include/video/common.h
@@ -60,5 +60,7 @@ const std::map<std::string, Color> color_by_name = {
 };
 
 const std::map<Color, cv::Scalar> scalar_by_color = {
-    {Color::Orange, cv::Scalar(30, 75, 250)}
+    {Color::Orange, cv::Scalar(30, 75, 250)},
+    {Color::Red, cv::Scalar(0, 0, 255)},
+    {Color::Green, cv::Scalar(0, 255, 0)}
 };
