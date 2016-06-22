adf6fb0166003c1d18488bea24207b175f1621f4
Max Sporyshev <coockoombra@gmail.com>
2016-06-22 13:13:38 +1000

    Фикс в wait_for

--- src/motion/motion_client/motion_client.cpp
+++ src/motion/motion_client/motion_client.cpp
@@ -56,8 +56,8 @@ void MotionClient::handle_msg_cmd_status(const motion::MsgCmdStatus& msg)
 CmdStatus MotionClient::wait_for(int id)
 {
     ipc::EventLoop loop(10);
-    while (cmd_history.find(id) == cmd_history.end()) {
-        loop.ok();
+    // этот цикл выходил слишком рано, из за loop.ok(), однако воспроизвести пока не получилось
+    while (cmd_history.find(id) == cmd_history.end() && loop.ok()) {
     }
     return cmd_history[id];
 }
