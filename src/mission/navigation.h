2f8657cba4144c3660c1b78f707f72b47c3284fc
bioniwulf <bioniwulf@gmail.com>
2016-06-29 15:00:28 +1000

    Исправлены сообщения в миссии.

--- src/mission/navigation.h
+++ src/mission/navigation.h
@@ -2,20 +2,20 @@
 
 #include <libipc/ipc.h>
 #include <navig/MsgDepth.h>
-#include <navig/MsgAngles.h>
-#include <navig/MsgVelocity.h>
+#include <navig/MsgAngle.h>
+#include <navig/MsgPlaneVelocity.h>
 
 class Navigation
 {
 public:
     Navigation(ipc::Communicator& comm)
-            : angle_sub_(comm.subscribe<navig::MsgAngles>("navig"))
+            : angle_sub_(comm.subscribe<navig::MsgAngle>("navig"))
             , depth_sub_(comm.subscribe<navig::MsgDepth>("navig"))
     {}
 
     double last_depth()
     {
-        return depth_sub_.msg_wait().depth;
+        return depth_sub_.msg_wait().distance;
     }
 
     double last_head()
@@ -24,6 +24,6 @@ public:
     }
 
 private:
-    ipc::Subscriber<navig::MsgAngles> angle_sub_;
+    ipc::Subscriber<navig::MsgAngle> angle_sub_;
     ipc::Subscriber<navig::MsgDepth> depth_sub_;
 };
