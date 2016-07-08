c178bee00ffcb9bd93f4e7bf8bb9dcfa946332f5
bioniwulf <bioniwulf@gmail.com>
2016-07-08 20:40:05 +1000

    Теперь в режиме симуляции драйвер камеры может публиковать кадры из заданной папки.

--- src/camera/include/avt_vimba_camera/simulator_camera.h
+++ src/camera/include/avt_vimba_camera/simulator_camera.h
@@ -1,15 +1,12 @@
 #pragma once 
 
-// #include <avt_vimba_camera/avt_vimba_camera.h>
-#include <avt_vimba_camera/AvtVimbaCameraConfig.h>
-// #include <avt_vimba_camera/avt_vimba_api.h>
-
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/CameraInfo.h>
 #include <camera_info_manager/camera_info_manager.h>
 #include <image_transport/image_transport.h>
-#include <dynamic_reconfigure/server.h>
+
+#include <cv_bridge/cv_bridge.h>
 
 #include <string>
 
@@ -20,42 +17,29 @@ class SimulatorCamera {
   ~SimulatorCamera(void);
 
  private:
-  // AvtVimbaApi api_;
-  // AvtVimbaCamera cam_;
-
-  // diagnostic_updater::Updater updater_;
-  // diagnostic_updater::TopicDiagnostic* pub_freq_;
-
   ros::NodeHandle nh_;
   ros::NodeHandle nhp_;
 
-  // std::string ip_;
-  // std::string guid_;
-  // std::string camera_info_url_;
-  // bool show_debug_prints_;
+  std::string ip_;
+  std::string guid_;
+  std::string camera_info_url_;
+  bool show_debug_prints_;
+
+  bool get_from_storage_;
+  std::string image_folder_;
+  std::vector<std::string> image_storage_;
 
   image_transport::ImageTransport it_;
   // ROS Camera publisher
   image_transport::CameraPublisher pub_;
-  sensor_msgs::CameraInfo ci_;
   ros::Timer timer_publish_;
 
-
   // sensor_msgs::CameraInfo left_info_;
-  // boost::shared_ptr<camera_info_manager::CameraInfoManager> info_man_;
+  boost::shared_ptr<camera_info_manager::CameraInfoManager> info_man_;
 
-  // Dynamic reconfigure
-  typedef avt_vimba_camera::AvtVimbaCameraConfig Config;
-  typedef dynamic_reconfigure::Server<Config> ReconfigureServer;
-  ReconfigureServer reconfigure_server_;
-
-  // Camera configuration
-  Config camera_config_;
-
-  // void frameCallback(const FramePtr& vimba_frame_ptr);
-  void configure(Config& newconfig, uint32_t level);
-  void updateCameraInfo(const Config& config);
   void publish_frame(const ros::TimerEvent& event);
   void create_sim_image(sensor_msgs::Image& output_image);
+  void get_storaged_image(sensor_msgs::Image& output_image);
+  void parse_image_storage();
 };
 }
