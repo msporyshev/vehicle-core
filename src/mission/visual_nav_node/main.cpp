9d0b65c78a70691ca40583592401cce1fa47235b
Max Sporyshev <coockoombra@gmail.com>
2016-07-13 10:52:47 +1000

    Добавлена навигация по перевернутым воротам

--- src/mission/visual_nav_node/main.cpp
+++ src/mission/visual_nav_node/main.cpp
@@ -7,6 +7,7 @@
 
 #include <mission/MsgOrangeLane.h>
 #include <mission/MsgValidationGate.h>
+#include <mission/MsgNavigateChannel.h>
 #include <vision/MsgFoundStripe.h>
 #include <vision/MsgFoundGate.h>
 
@@ -21,10 +22,65 @@ using namespace utils;
 
 ros::Publisher orange_lane_pub;
 ros::Publisher validation_gate_pub;
+ros::Publisher navigate_channel_pub;
 
 shared_ptr<Odometry> odometry;
 CameraModel front_camera = CameraModel::create_front_camera();
 CameraModel bottom_camera = CameraModel::create_bottom_camera();
+
+void receive_navigate_channel(const MsgFoundGate& msg)
+{
+    if (msg.gate.empty()) {
+        return;
+    }
+
+    YamlReader cfg("navigate_channel_task.yml", "mission");
+    double gate_real_size = cfg.read_as<double>("channel_real_size");
+
+    MsgNavigateChannel current_channel;
+
+    odometry->add_frame_odometry(msg.odometry);
+
+
+    auto left = msg.gate.front().left;
+    auto right = msg.gate.front().right;
+
+    auto lbegin = front_camera.frame_coord(left.begin);
+    auto lend = front_camera.frame_coord(left.end);
+    if (lbegin.y < lend.y) {
+        swap(lbegin, lend);
+    }
+
+    auto rbegin = front_camera.frame_coord(right.begin);
+    auto rend = front_camera.frame_coord(right.end);
+    if (rbegin.y < rend.y) {
+        swap(rbegin, rend);
+    }
+
+    auto p1 = (lbegin + lend) * 0.5;
+    auto p2 = (rbegin + rend) * 0.5;
+
+    double center = (p1.x + p2.x) / 2;
+
+    current_channel.frame_number = msg.frame_number;
+
+    current_channel.left_bottom = lend.y;
+    current_channel.right_bottom = rend.y;
+    current_channel.width_ratio = std::abs(p2.x - p2.x) / front_camera.get_w();
+
+    current_channel.center = (p1 + p2) * 0.5;
+    double heading_delta = front_camera.heading_to_point(current_channel.center);
+    current_channel.bearing = heading_delta;
+    current_channel.direction =
+        normalize_degree_angle(heading_delta + odometry->frame_head());
+
+    current_channel.distance = front_camera.calc_dist_to_object(gate_real_size, p1, p2);
+    current_channel.position = odometry->front_target_pos(gate_real_size, p1, p2, current_channel.center);
+
+    current_channel.odometry = msg.odometry;
+
+    navigate_channel_pub.publish(current_channel);
+}
 
 void receive_orange_stripe(const MsgFoundStripe& msg)
 {
@@ -58,6 +114,8 @@ void receive_orange_stripe(const MsgFoundStripe& msg)
     double frame_width = norm(wbegin - wend);
     current_lane.position =
         odometry->bottom_target_pos(real_lane_width, frame_width, current_lane.center);
+
+    current_lane.odometry = msg.odometry;
 
     orange_lane_pub.publish(current_lane);
 }
@@ -101,6 +159,8 @@ void receive_validation_gate(const MsgFoundGate& msg)
 
     current_gate.distance = front_camera.calc_dist_to_object(gate_real_size, p1, p2);
     current_gate.position = odometry->front_target_pos(gate_real_size, p1, p2, current_gate.center);
+
+    current_gate.odometry = msg.odometry;
 
     validation_gate_pub.publish(current_gate);
 }
