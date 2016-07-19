2f52fd43cb92adb2dff7fb65da6add947024c4c5
Max Sporyshev <coockoombra@gmail.com>
2016-07-19 18:10:31 +1000

    Изменения в сообщениях и использовании модели камеры при навигации по объектом с изображений

--- src/mission/visual_nav_node/main.cpp
+++ src/mission/visual_nav_node/main.cpp
@@ -8,8 +8,12 @@
 #include <mission/MsgOrangeLane.h>
 #include <mission/MsgValidationGate.h>
 #include <mission/MsgNavigateChannel.h>
+#include <mission/MsgRedBuoy.h>
+
 #include <vision/MsgFoundStripe.h>
 #include <vision/MsgFoundGate.h>
+#include <vision/MsgFoundCircle.h>
+#include <vision/MsgFoundBin.h>
 
 #include <dsp/MsgBeacon.h>
 #include <mission/MsgPingerPosition.h>
@@ -20,21 +24,100 @@
 
 using namespace vision;
 using namespace mission;
+using namespace navig;
 using namespace std;
 using namespace utils;
 
 ros::Publisher orange_lane_pub;
 ros::Publisher validation_gate_pub;
 ros::Publisher navigate_channel_pub;
+ros::Publisher red_buoy_pub;
 ros::Publisher pinger_position_pub;
 
 shared_ptr<Odometry> odometry;
-CameraModel front_camera = CameraModel::create_front_camera();
-CameraModel bottom_camera = CameraModel::create_bottom_camera();
+FrontCamera front_camera;
+BottomCamera bottom_camera;
 
 dsp::MsgBeacon current_dsp_msg_;
 int is_bearing_received_ = 0;
 double trusted_coef_ = 0.1;
+
+MsgObjectPosition calc_object_position(
+        const CameraModel& camera,
+        const MsgOdometry odometry,
+        double real_size,
+        Point2d start,
+        Point2d end,
+        Point2d center
+        )
+{
+    MsgObjectPosition pos;
+
+    pos.position = odometry.pos + camera.navig_offset_to_object(
+        odometry.angle.heading, real_size, start, end, center);
+    pos.bearing = camera.bearing_to_point(center);
+    pos.direction = normalize_degree_angle(pos.bearing + odometry.angle.heading);
+    pos.distance = camera.calc_dist_to_object(real_size, start, end);
+
+    return pos;
+}
+
+MsgObjectPosition calc_object_position(
+        const CameraModel& camera,
+        const MsgOdometry odometry,
+        double real_size,
+        int pixel_size,
+        Point2d pixel
+        )
+{
+    MsgObjectPosition pos;
+
+    pos.position = odometry.pos + camera.navig_offset_to_object(
+        odometry.angle.heading, real_size, pixel_size, pixel);
+    pos.bearing = camera.bearing_to_point(camera.frame_coord(pixel));
+    pos.direction = normalize_degree_angle(pos.bearing + odometry.angle.heading);
+    pos.distance = camera.calc_dist_to_object(real_size, pixel_size);
+
+    return pos;
+}
+
+void receive_bins(const MsgFoundBin& msg)
+{
+    if (msg.bins.empty()) {
+        return;
+    }
+}
+
+void receive_buoy(const MsgFoundCircle& msg)
+{
+    if (msg.circles.empty()) {
+        return;
+    }
+
+    MsgCircle red_circle;
+    for (auto& circle : msg.circles) {
+        if (circle.radius > red_circle.radius) {
+            red_circle = circle;
+        }
+    }
+
+    YamlReader cfg("buoy_task.yml", "mission");
+    double buoy_real_size = cfg.read_as<double>("buoy_real_size");
+
+    MsgRedBuoy current_buoy;
+
+    current_buoy.pixel_size = red_circle.radius;
+    current_buoy.size_ratio = red_circle.radius / front_camera.get_w();
+
+    current_buoy.center = front_camera.frame_coord(red_circle.center);
+
+    current_buoy.pos = calc_object_position(front_camera, msg.odometry,
+        buoy_real_size, red_circle.radius, red_circle.center);
+    current_buoy.depth = msg.odometry.depth.distance
+        + front_camera.calc_depth_to_object(buoy_real_size, red_circle.radius, red_circle.center);
+
+    current_buoy.odometry = msg.odometry;
+}
 
 void receive_navigate_channel(const MsgFoundGate& msg)
 {
@@ -48,7 +131,6 @@ void receive_navigate_channel(const MsgFoundGate& msg)
     MsgNavigateChannel current_channel;
 
     odometry->add_frame_odometry(msg.odometry);
-
 
     auto left = msg.gate.front().left;
     auto right = msg.gate.front().right;
@@ -80,16 +162,12 @@ void receive_navigate_channel(const MsgFoundGate& msg)
     current_channel.right_bottom = rend.y;
     current_channel.width_ratio = std::abs(p2.x - p2.x) / front_camera.get_w();
 
-    current_channel.direction_left = front_camera.heading_to_point(p1);
+    current_channel.direction_left =
+        normalize_degree_angle(odometry->frame_head() + front_camera.bearing_to_point(p1));
 
     current_channel.center = (p1 + p2) * 0.5;
-    double heading_delta = front_camera.heading_to_point(current_channel.center);
-    current_channel.bearing = heading_delta;
-    current_channel.direction =
-        normalize_degree_angle(heading_delta + odometry->frame_head());
-
-    current_channel.distance = front_camera.calc_dist_to_object(gate_real_size, p1, p2);
-    current_channel.position = odometry->front_target_pos(gate_real_size, p1, p2, current_channel.center);
+    current_channel.pos = calc_object_position(front_camera, msg.odometry,
+        gate_real_size, p1, p2, current_channel.center);
 
     current_channel.odometry = msg.odometry;
 
@@ -120,16 +198,11 @@ void receive_orange_stripe(const MsgFoundStripe& msg)
     MsgOrangeLane current_lane;
     current_lane.center = (begin + end) * 0.5;
 
-    current_lane.distance = bottom_camera.calc_dist_to_object(real_lane_width, wbegin, wend);
-
+    current_lane.pos = calc_object_position(bottom_camera, msg.odometry, real_lane_width,
+        wbegin, wend, current_lane.center);
     double bearing = M_PI_2 - atan2(begin.y - end.y, begin.x - end.x);
-    current_lane.bearing = to_deg(normalize_angle(bearing));
-    current_lane.direction = normalize_degree_angle(
-        odometry->frame_head() + current_lane.bearing);
-
-    double frame_width = norm(wbegin - wend);
-    current_lane.position =
-        odometry->bottom_target_pos(real_lane_width, frame_width, current_lane.center);
+    current_lane.pos.bearing = utils::to_deg(normalize_angle(bearing));
+    current_lane.pos.direction = normalize_degree_angle(msg.odometry.angle.heading + current_lane.pos.bearing);
 
     current_lane.odometry = msg.odometry;
 
@@ -168,13 +241,9 @@ void receive_validation_gate(const MsgFoundGate& msg)
     current_gate.width_ratio = std::abs(x2_ - x1_) / front_camera.get_w();
 
     current_gate.center = (p1 + p2) * 0.5;
-    double heading_delta = front_camera.heading_to_point(current_gate.center);
-    current_gate.bearing = heading_delta;
-    current_gate.direction =
-        normalize_degree_angle(heading_delta + odometry->frame_head());
 
-    current_gate.distance = front_camera.calc_dist_to_object(gate_real_size, p1, p2);
-    current_gate.position = odometry->front_target_pos(gate_real_size, p1, p2, current_gate.center);
+    current_gate.pos = calc_object_position(front_camera, msg.odometry,
+        gate_real_size, p1, p2, current_gate.center);
 
     current_gate.odometry = msg.odometry;
 
@@ -192,12 +261,12 @@ void calculate_pinger_coordinates(const ros::TimerEvent&)
     static double weight_old = 0;
     static double weight_north_old = 0;
     static double weight_east_old = 0;
-    
+
     double north = odometry->pos().north;
     double east  = odometry->pos().east;
 
     double pinger_north = north + current_dsp_msg_.distance * cos(to_rad(current_dsp_msg_.heading));
-    double pinger_east  = east + current_dsp_msg_.distance * sin(to_rad(current_dsp_msg_.heading));    
+    double pinger_east  = east + current_dsp_msg_.distance * sin(to_rad(current_dsp_msg_.heading));
 
     double weight       = weight_old * (1 - trusted_coef_) + trusted_coef_ * is_bearing_received_;
     double weight_north = weight_north_old * (1 - trusted_coef_) + pinger_north * trusted_coef_ * is_bearing_received_;
@@ -228,12 +297,14 @@ int main(int argc, char* argv[])
     orange_lane_pub = comm.advertise_cmd<MsgOrangeLane>("mission");
     validation_gate_pub = comm.advertise_cmd<MsgValidationGate>("mission");
     navigate_channel_pub = comm.advertise_cmd<MsgNavigateChannel>("mission");
+    red_buoy_pub = comm.advertise_cmd<MsgRedBuoy>("mission");
 
     pinger_position_pub = comm.advertise_cmd<MsgPingerPosition>("mission");
 
     auto orange_stripe_sub = comm.subscribe("vision", receive_orange_stripe);
     auto validation_gate_sub = comm.subscribe("vision", receive_validation_gate);
     auto navigate_channel_sub = comm.subscribe("vision", receive_navigate_channel);
+    auto buoy_sub = comm.subscribe("vision", receive_buoy);
 
     auto pinger_sub = comm.subscribe("dsp", receive_pinger_bearing);
 
