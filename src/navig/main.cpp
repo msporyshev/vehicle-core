df92a72b25434a659d5190596f5ad8a718b17c98
pavin <pavin@bk.ru>
2016-08-16 17:06:17 +1000

    Добавил републикацию глобальных координат от GPS'а (MsgGlobalPosition)

--- src/navig/main.cpp
+++ src/navig/main.cpp
@@ -2,7 +2,7 @@
 \file
 \brief Навигационный модуль расчета навигационных данных аппарата
 
-Модуль содержит подписку на сообщения сенсорных устройств и
+Модуль содержит подписку на сообщения сенсорных устройств
 и основной цикл навига, в котором публикуются навигационные данные
 */
 
@@ -20,6 +20,7 @@
 #include <compass/MsgAngle.h>           ///< Курс, дифферент, крен от компаса
 #include <compass/MsgAngleRate.h>       ///< Скорость курса, дифферента и крена от компаса
 #include <compass/MsgAcceleration.h>    ///< Ускорения вперед, вправо и вниз от компаса
+#include <gps/MsgGlobalPosition.h>      ///< Глобальные координаты от GPS
 
 // Публикуемые сообщения
 #include <navig/MsgDepth.h>             ///< Глубина и скорость изменения глубины
@@ -28,6 +29,7 @@
 #include <navig/MsgAngle.h>             ///< Курс, дифферент, крен
 #include <navig/MsgAngleRate.h>         ///< Скорость курса, дифферента и крена
 #include <navig/MsgLocalPosition.h>     ///< Локальные координаты аппарата (север, восток) в метрах
+#include <navig/MsgGlobalPosition.h>    ///< Глобальные координаты аппарата (широта, долгота) в градусах
 #include <navig/MsgRaw.h>               ///< Сырые отладочные данные навига
 #include <navig/MsgOdometry.h>          ///< Публикуемое обобщенное сообщение телеметрии с флагами
 
@@ -50,26 +52,29 @@ int main(int argc, char* argv[])
     auto sub_compass_angle        = subscribe<compass::MsgAngle>("compass");
     auto sub_compass_angle_rate   = subscribe<compass::MsgAngleRate>("compass");
     auto sub_compass_acceleration = subscribe<compass::MsgAcceleration>("compass");
+    auto sub_gps_global_position  = subscribe<gps::MsgGlobalPosition>("gps");
 
     // Подписываемся на публикацию сообщений
-    auto pub_navig_depth          = advertise<navig::MsgDepth>();
-    auto pub_navig_height         = advertise<navig::MsgHeight>();
-    auto pub_navig_plane_velocity = advertise<navig::MsgPlaneVelocity>();
-    auto pub_navig_angle          = advertise<navig::MsgAngle>();
-    auto pub_navig_angle_rate     = advertise<navig::MsgAngleRate>();
-    auto pub_navig_local_position = advertise<navig::MsgLocalPosition>();
-    auto pub_navig_odometry       = advertise<navig::MsgOdometry>();
-    auto pub_navig_raw            = advertise<navig::MsgRaw>();
+    auto pub_navig_depth           = advertise<navig::MsgDepth>();
+    auto pub_navig_height          = advertise<navig::MsgHeight>();
+    auto pub_navig_plane_velocity  = advertise<navig::MsgPlaneVelocity>();
+    auto pub_navig_angle           = advertise<navig::MsgAngle>();
+    auto pub_navig_angle_rate      = advertise<navig::MsgAngleRate>();
+    auto pub_navig_local_position  = advertise<navig::MsgLocalPosition>();
+    auto pub_navig_global_position = advertise<navig::MsgGlobalPosition>();
+    auto pub_navig_odometry        = advertise<navig::MsgOdometry>();
+    auto pub_navig_raw             = advertise<navig::MsgRaw>();
 
     // Заводим переменные публикуемых данных
-    navig::MsgDepth         navig_depth;
-    navig::MsgHeight        navig_height;
-    navig::MsgPlaneVelocity navig_plane_velocity;
-    navig::MsgAngle         navig_angle;
-    navig::MsgAngleRate     navig_angle_rate;
-    navig::MsgLocalPosition navig_local_position;
-    navig::MsgOdometry odometry;
-    navig::MsgRaw           raw;
+    navig::MsgDepth          navig_depth;
+    navig::MsgHeight         navig_height;
+    navig::MsgPlaneVelocity  navig_plane_velocity;
+    navig::MsgAngle          navig_angle;
+    navig::MsgAngleRate      navig_angle_rate;
+    navig::MsgLocalPosition  navig_local_position;
+    navig::MsgGlobalPosition navig_global_position;
+    navig::MsgOdometry       odometry;
+    navig::MsgRaw            raw;
 
     // Ожидаем пока включатся и начнут публиковать остальные ноды
     ros::Duration(START_DELAY).sleep();
@@ -91,6 +96,7 @@ int main(int argc, char* argv[])
         auto compass_angle        = sub_compass_angle       .msg();
         auto compass_angle_rate   = sub_compass_angle_rate  .msg();
         auto compass_acceleration = sub_compass_acceleration.msg();
+        auto gps_global_position  = sub_gps_global_position .msg();
 
         // Заполняем служебные поля в отладочном сообщении
         raw.real_period = dt;
@@ -100,6 +106,7 @@ int main(int argc, char* argv[])
         update_age_info(raw.compass_angle       , sub_compass_angle       .age(), AGE_MAX);
         update_age_info(raw.compass_angle_rate  , sub_compass_angle_rate  .age(), AGE_MAX);
         update_age_info(raw.compass_acceleration, sub_compass_acceleration.age(), AGE_MAX);
+        update_age_info(raw.gps_global_position , sub_gps_global_position .age(), AGE_MAX);
 
         // Интегрирование вектора СКОРОСТИ ПО УСКОРЕНИЯМ (только если приходят ускорения)
         if(raw.compass_acceleration.fresh) {
@@ -122,29 +129,32 @@ int main(int argc, char* argv[])
         }
 
         // ЗАПОЛНЯЕМ ВСЕ СООБЩЕНИЯ, кроме уже заполненного сырого
-        navig_depth.distance         = supervisor_depth.distance;
-        navig_depth.velocity         = supervisor_depth.velocity;
-        navig_height.distance        = dvl_down.distance;           /// \todo Сделать пересчет с учетом крена/дифферента/ЭЛС
-        navig_height.velocity        = dvl_down.velocity;           /// \todo Сделать пересчет с учетом крена/дифферента/ЭЛС
-        navig_angle.heading          = compass_angle.heading;
-        navig_angle.pitch            = compass_angle.pitch;
-        navig_angle.roll             = compass_angle.roll;
-        navig_angle_rate.heading     = compass_angle_rate.heading;
-        navig_angle_rate.pitch       = compass_angle_rate.pitch;
-        navig_angle_rate.roll        = compass_angle_rate.roll;
-        navig_plane_velocity.right   = raw.velocity.right;
-        navig_plane_velocity.forward = raw.velocity.forward;
-        navig_local_position.east    = raw.position.east;
-        navig_local_position.north   = raw.position.north;
+        navig_depth.distance            = supervisor_depth.distance;
+        navig_depth.velocity            = supervisor_depth.velocity;
+        navig_height.distance           = dvl_down.distance;           /// \todo Сделать пересчет с учетом крена/дифферента/ЭЛС
+        navig_height.velocity           = dvl_down.velocity;           /// \todo Сделать пересчет с учетом крена/дифферента/ЭЛС
+        navig_angle.heading             = compass_angle.heading;
+        navig_angle.pitch               = compass_angle.pitch;
+        navig_angle.roll                = compass_angle.roll;
+        navig_angle_rate.heading        = compass_angle_rate.heading;
+        navig_angle_rate.pitch          = compass_angle_rate.pitch;
+        navig_angle_rate.roll           = compass_angle_rate.roll;
+        navig_plane_velocity.right      = raw.velocity.right;
+        navig_plane_velocity.forward    = raw.velocity.forward;
+        navig_local_position.east       = raw.position.east;
+        navig_local_position.north      = raw.position.north;
+        navig_global_position.latitude  = gps_global_position.latitude;
+        navig_global_position.longitude = gps_global_position.longitude;
 
         // Всегда публикуем сырые данные, а основные - только если они свежие
         pub_navig_raw.publish(raw);
-        if(raw.supervisor_depth  .fresh) pub_navig_depth         .publish(navig_depth);
-        if(raw.dvl_down          .fresh) pub_navig_height        .publish(navig_height);
-        if(raw.compass_angle     .fresh) pub_navig_angle         .publish(navig_angle);
-        if(raw.compass_angle_rate.fresh) pub_navig_angle_rate    .publish(navig_angle_rate);
-        if(raw.velocity_flag           ) pub_navig_plane_velocity.publish(navig_plane_velocity);
-        if(raw.position_flag           ) pub_navig_local_position.publish(navig_local_position);
+        if(raw.supervisor_depth   .fresh) pub_navig_depth          .publish(navig_depth);
+        if(raw.dvl_down           .fresh) pub_navig_height         .publish(navig_height);
+        if(raw.compass_angle      .fresh) pub_navig_angle          .publish(navig_angle);
+        if(raw.compass_angle_rate .fresh) pub_navig_angle_rate     .publish(navig_angle_rate);
+        if(raw.velocity_flag            ) pub_navig_plane_velocity .publish(navig_plane_velocity);
+        if(raw.position_flag            ) pub_navig_local_position .publish(navig_local_position);
+        if(raw.gps_global_position.fresh) pub_navig_global_position.publish(navig_global_position);
 
         // Публикуем обобщенное сообщение телеметрии с флагами
         odometry.has_angle    = raw.compass_angle.fresh;
